65727e3eb43e4b3516f5ab47121eb3f451e739b6
Claudio Cruz <claudio.dacruz@gmail.com>
2022-04-29 15:29:04 -0400

    Creation de fenetre blanche

--- ChefBurger/ChefBurger/ChefBurger.cpp
+++ ChefBurger/ChefBurger/ChefBurger.cpp
@@ -1,10 +1,32 @@
 // ChefBurger.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
 //
 
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main()
-{
-    std::cout << "Chef Burguer\n";
+using namespace sf;	//en mettant ce namespace, ça évite des sf::
+
+int main() {
+	RenderWindow window(VideoMode(800, 600), "Lost");
+	Event event;
+	RectangleShape fondEcran;
+
+	window.setFramerateLimit(60);
+
+	fondEcran.setSize(Vector2f(800, 600));
+	fondEcran.setFillColor(Color::White);
+
+	
+
+	while (window.isOpen()) {
+		while (window.pollEvent(event)) {
+			if (event.type == Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(fondEcran);
+		window.display();
+	}
+
+	return 0;
 }
-
